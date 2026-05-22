5650a3218df05ec780a4a00a55ee4fd5d20a3a5a
kairin <nekohui.chan@outlook.com>
2026-05-22 10:50:21 +0800

    Add eventSource and event_types imports

--- index.js
+++ index.js
@@ -13,3 +13,4 @@ jQuery(async () => {
 import { extension_settings, getContext, saveSettingsDebounced } from '../../../extensions.js';
 import { executeSlashCommandsWithOptions } from '../../../slash-commands.js';
 import { getPresetManager } from '../../../../script.js';
+import { eventSource, event_types } from '../../../../script.js';
