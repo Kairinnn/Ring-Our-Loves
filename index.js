6e67f67fc3660b8168b0f6e5e93e461d8a18ece5
kairin <nekohui.chan@outlook.com>
2026-05-22 10:48:20 +0800

    Import executeSlashCommandsWithOptions in index.js

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ jQuery(async () => {
     console.log('[RingOurLuv] settings:', extension_settings[extensionName]);
 });
 import { extension_settings, getContext, saveSettingsDebounced } from '../../../extensions.js';
+import { executeSlashCommandsWithOptions } from '../../../slash-commands.js';
