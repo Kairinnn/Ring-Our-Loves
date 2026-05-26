5e39f664d74b0dd8867a82825e7aebf5859eadd4
kairin <nekohui.chan@outlook.com>
2026-05-26 09:04:58 +0800

    Import marked library in index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { marked } from './marked.min.js';
 import { saveSettingsDebounced, eventSource, event_types, getRequestHeaders } from '../../../../script.js';
 import { extension_settings, getContext } from '../../../extensions.js';
 import { getPresetManager } from '../../../preset-manager.js';
